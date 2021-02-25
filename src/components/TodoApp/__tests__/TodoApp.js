4a6ea807397d7aa89035290ffdbc164855662ca6
lipengzhou <lpzmail@163.com>
2021-02-25 20:52:03 +0800

    update

--- src/components/TodoApp/__tests__/TodoApp.js
+++ src/components/TodoApp/__tests__/TodoApp.js
@@ -11,12 +11,12 @@ beforeEach(async () => {
     {
       id: 1,
       text: 'eat',
-      done: false
+      done: true
     },
     {
       id: 2,
       text: 'sleep',
-      done: false
+      done: true
     },
     {
       id: 3,
@@ -37,19 +37,21 @@ test('展示所有任务列表正常', async () => {
   expect(wrapper.find('.todo-list').findAll('li').length).toBe(wrapper.vm.todos.length)
 })
 
-// test('删除单个任务正常', async () => {
-//   const list = wrapper.find('.todo-list').findAll('li')
-//   for (let i = 0; i < list.length; i++) {
-//     const li = list.at(i)
-//     await li.find('button[class="destroy"]').trigger('click')
-//     // 确认 list 中确实没有 button 那个 li 了
-//   }
-//   await wrapper.find('button[class="destroy"]').trigger('click')
-//   expect(wrapper.find('.todo-list').find('li').exists()).toBeFalsy()
-// })
+test('删除单个任务正常', async () => {
+  const list = wrapper.find('.todo-list').findAll('li')
+  await list.at(0).find('button[class="destroy"]').trigger('click')
+  expect(wrapper.vm.todos.length).toBe(2)
+})
 
-test('删除所有已完成任务正常', () => {
-  console.log('正常')
+test('删除所有已完成任务正常', async () => {
+  await wrapper.find('button[class="clear-completed"]').trigger('click')
+  expect(wrapper.vm.todos).toEqual([
+    {
+      id: 3,
+      text: 'play',
+      done: false
+    }
+  ])
 })
 
 test('切换单个任务完成状态正常', async () => {
@@ -66,26 +68,128 @@ test('切换单个任务完成状态正常', async () => {
   }
 })
 
-test('切换所有任务完成状态正常', () => {
-  console.log('正常')
+test('展示所有任务完成状态正常', async () => {
+  // 所有任务已完成则 toggleAll 是选中状态
+  const list = wrapper.findAll('input[class="toggle"]')
+  const toggleAll = wrapper.find('input[class="toggle-all"]')
+  for (let i = 0; i < list.length; i++) {
+    list.at(i).setChecked(true)
+  }
+  await Vue.nextTick()
+  expect(toggleAll.element.checked).toBeTruthy()
+
+  // 只要有1个任务没有完成，则 toggleAll 是未选中状态
+  await list.at(0).setChecked(false)
+  expect(toggleAll.element.checked).toBeFalsy()
 })
 
-test('展示未完成任务数量正常', () => {
-  console.log('正常')
+test('切换所有任务完成状态正常', async () => {
+  const toggleAll = wrapper.find('input[class="toggle-all"]')
+  const list = wrapper.findAll('input[class="toggle"]')
+
+  // 全部完成
+  await toggleAll.setChecked(true)
+  for (let i = 0; i < list.length; i++) {
+    expect(list.at(i).element.checked).toBeTruthy()
+  }
+
+  // 全部未完成
+  await toggleAll.setChecked(false)
+  for (let i = 0; i < list.length; i++) {
+    expect(list.at(i).element.checked).toBeFalsy()
+  }
 })
 
-test('双击任务获得编辑状态正常', () => {
-  console.log('正常')
+test('展示所有未完成任务数量正常', () => {
+  const count = wrapper.vm.todos.filter(t => !t.done).length
+  const showCount = wrapper.find('span[class="todo-count"]').find('strong').element.innerHTML
+  expect(showCount - 0).toBe(count)
 })
 
-test('编辑任务正常', () => {
-  console.log('正常')
+describe('编辑任务', () => {
+  test('双击任务获得编辑状态正常', async () => {
+    const list = wrapper.find('.todo-list').findAll('li')
+    for (let i = 0; i < list.length; i++) {
+      await list.at(i).find('label').trigger('dblclick')
+      expect(list.at(i).classes()).toContain('editing')
+    }
+  })
+
+  test('编辑文本框展示正常', () => {
+    const list = wrapper.find('.todo-list').findAll('li')
+    for (let i = 0; i < list.length; i++) {
+      const editInput = list.at(i).find('input[class="edit"]')
+      const label = list.at(i).find('label')
+      expect(editInput.element.value).toBe(label.element.innerHTML)
+    }
+  })
+
+  test('取消编辑正常', async () => {
+    const list = wrapper.find('.todo-list').findAll('li')
+    for (let i = 0; i < list.length; i++) {
+      await list.at(i).find('label').trigger('dblclick') // 获得编辑状态
+      const text = `text${i}`
+      const editInput = list.at(i).find('input[class="edit"]')
+      const originText = editInput.element.value
+      await editInput.setValue(text)
+      await editInput.trigger('keyup.esc') // 必须是编辑显示状态才可以触发键盘事件
+      const label = list.at(i).find('label')
+      expect(label.element.innerHTML).toBe(originText)
+    }
+  })
+
+  test('确认编辑正常', async () => {
+    const list = wrapper.find('.todo-list').findAll('li')
+    for (let i = 0; i < list.length; i++) {
+      await list.at(i).find('label').trigger('dblclick')
+      const text = `text${i}`
+      const editInput = list.at(i).find('input[class="edit"]')
+      await editInput.setValue(text)
+      await editInput.trigger('keyup.enter') // 必须是编辑显示状态才可以触发键盘事件
+      const label = list.at(i).find('label')
+      expect(label.element.innerHTML).toBe(text)
+    }
+  })
 })
 
-test('筛选展示所有未完成任务列表正常', () => {
-  console.log('筛选展示所有未完成任务列表正常')
+test.only('筛选展示所有未完成任务列表正常', async () => {
+  // console.log(jest.jsdom)
+  // delete window.location
+  // window.location = {
+  //   ...window.location
+  // }
+  // document.location.href = 'xxx'
+  // Object.defineProperty(window.location, 'href', {
+  //   writable: true,
+  //   value: 'https://www.somthing.com/test.html?query=true'
+  // })
+  // Object
+  // jest.
+  // console.log(window.location.hash)
+  // window.location.hash = '#/active'
+  // window.location.href = '/#/active'
+  // window.location.foo = 'bar'
+  // Object.defineProperty(window, 'location', {
+  //   value: {
+  //     hash: 'abc'
+  //   },
+  //   writable: true
+  // })
+  // console.log(123)
+  // await Promise.resolve(r => r())
+  // jest.useFakeTimers()
+  // const fn = jest.fn(() => {
+
+  // })
+
+  // await wrapper.vm.handleHashchange()
+  // await Vue.nextTick()
+  // expect(wrapper.find('.todo-list').findAll('li').length).toBe(1)
+  // 视图中的任务列表是对的
+  // a 链接样式正确
 })
 
 test('筛选展示所有已完成任务列表正常', () => {
+  window.location.hash = '#/completed'
   console.log('正常')
 })
--- src/components/TodoApp/index.vue
+++ src/components/TodoApp/index.vue
@@ -5,27 +5,36 @@
     />
     <!-- This section should be hidden by default and shown when there are todos -->
     <section class="main">
-      <input id="toggle-all" class="toggle-all" type="checkbox" />
+      <input v-model="toggleAll" id="toggle-all" class="toggle-all" type="checkbox" />
       <label for="toggle-all">Mark all as complete</label>
       <ul class="todo-list">
         <!-- These are here just to show the structure of the list items -->
         <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
         <li
-          v-for="todo in todos"
+          v-for="todo in filterTodos"
           :key="todo.id"
           :class="{
-            completed: todo.done
+            completed: todo.done,
+            editing: todo === editTodo
           }"
         >
           <div class="view">
             <input class="toggle" type="checkbox" v-model="todo.done" />
-            <label>{{ todo.text }}</label>
+            <label
+              @dblclick="handleShowEditing(todo)"
+            >{{ todo.text }}</label>
             <button
               class="destroy"
               @click="handleDestroyTodo(todo)"
             ></button>
           </div>
-          <input class="edit" value="Create a TodoMVC template" />
+          <input
+            class="edit"
+            :value="todo.text"
+            @keyup.enter="handleEdit(todo, $event)"
+            @blur="handleEdit(todo, $event)"
+            @keyup.esc="handleCancelEdit"
+          />
         </li>
         <!-- <li>
           <div class="view">
@@ -40,21 +49,21 @@
     <!-- This footer should be hidden by default and shown when there are todos -->
     <footer class="footer">
       <!-- This should be `0 items left` by default -->
-      <span class="todo-count"><strong>0</strong> item left</span>
+      <span class="todo-count"><strong>{{ remainingCount }}</strong> item left</span>
       <!-- Remove this if you don't implement routing -->
       <ul class="filters">
         <li>
-          <a class="selected" href="#/">All</a>
+          <a :class="{ selected: hash === '/' }" href="#/">All</a>
         </li>
         <li>
-          <a href="#/active">Active</a>
+          <a :class="{ selected: hash === '/active' }" href="#/active">Active</a>
         </li>
         <li>
-          <a href="#/completed">Completed</a>
+          <a :class="{ selected: hash === '/completed' }" href="#/completed">Completed</a>
         </li>
       </ul>
       <!-- Hidden if no completed items are left ↓ -->
-      <button class="clear-completed">Clear completed</button>
+      <button class="clear-completed" @click="handleClearCompleted">Clear completed</button>
     </footer>
   </section>
 </template>
@@ -69,8 +78,46 @@ export default {
   },
   data () {
     return {
-      todos: [] // 任务列表
+      todos: [
+        {
+          id: 1,
+          text: 'eat',
+          done: true
+        },
+        {
+          id: 2,
+          text: 'sleep',
+          done: true
+        },
+        {
+          id: 3,
+          text: 'play',
+          done: false
+        }
+      ], // 任务列表
+      filterTodos: [], // 筛选的任务列表
+      editTodo: null, // 当前编辑的任务
+      hash: '/'
     }
+  },
+  computed: {
+    remainingCount () {
+      return this.todos.filter(t => !t.done).length
+    },
+    toggleAll: {
+      get () {
+        return this.todos.every(t => t.done)
+      },
+      set (value) {
+        this.todos.forEach(t => {
+          t.done = value
+        })
+      }
+    }
+  },
+  created () {
+    this.handleHashchange()
+    window.onhashchange = this.handleHashchange.bind(this)
   },
   methods: {
     handleNewTodo (text) {
@@ -90,6 +137,57 @@ export default {
         if (index !== -1) {
           this.todos.splice(index, 1)
         }
+      }
+    },
+
+    handleClearCompleted () {
+      for (let i = 0; i < this.todos.length; i++) {
+        const todo = this.todos[i]
+        if (todo.done) {
+          this.todos.splice(i, 1)
+          i--
+        }
+      }
+    },
+
+    handleShowEditing (todo) {
+      this.editTodo = todo
+    },
+
+    handleEdit (todo, e) {
+      const text = e.target.value
+      if (text.trim().length) {
+        // 修改
+        todo.text = text
+
+        // 取消编辑状态
+        this.editTodo = null
+      } else {
+        // 删除
+        this.handleDestroyTodo(todo)
+      }
+    },
+
+    handleCancelEdit () {
+      this.editTodo = null
+    },
+
+    handleHashchange () {
+      console.log('handleHashchange', document.location.href)
+      const href = window.location.hash.substring(1)
+      switch (href) {
+        case '/active':
+          this.filterTodos = this.todos.filter(t => !t.done)
+          this.hash = '/active'
+          break
+        case '/completed':
+          this.filterTodos = this.todos.filter(t => t.done)
+          this.hash = '/completed'
+          break
+        default:
+          this.filterTodos = this.todos
+          this.hash = '/'
+          break
       }
     }
   }
